bd11fd180e94691ec87e73ef13b9717914aaf30e
Sung Jae Cho <rubycho@outlook.kr>
2020-02-15 10:16:15 +0900

    fix production settings

--- webpi/settings.py
+++ webpi/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'v*#k0^*gcnodu(c%r!0akf^!qes_c4ni^8^ggs@m(a=jad3(2p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
